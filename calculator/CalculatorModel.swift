fca0f791fb6ae0306da6c06dc59ee8b53b5bc6c7
mbresnan1701 <bresnan.mw@gmail.com>
2017-08-16 22:40:38 -0700

    add model, update ui

---
+++ calculator/CalculatorModel.swift
@@ -0,0 +1,129 @@
+//
+//  CalculatorModel.swift
+//  calculator
+//
+//  Created by Matthew Bresnan on 8/8/17.
+//  Copyright © 2017 Matthew Bresnan. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorModel {
+    private var accumulator: Double?
+    
+    private var pendingBinaryOp: PendingBinaryOperation?
+    
+    var result: Double? {
+        get {
+            return accumulator
+        }
+    }
+    
+    var description: String = ""
+
+    
+    var resultIsPending: Bool {
+        get {
+            return pendingBinaryOp != nil
+        }
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "π": Operation.constant(Double.pi),
+        "e": Operation.constant(M_E),
+        "√": Operation.unaryOperation(sqrt),
+        "sin": Operation.unaryOperation(sin),
+        "cos": Operation.unaryOperation(cos),
+        "tan": Operation.unaryOperation(tan),
+        "+": Operation.binaryOperation(+),
+        "-": Operation.binaryOperation(-),
+        "*": Operation.binaryOperation(*),
+        "÷": Operation.binaryOperation(/),
+        "=": Operation.equals,
+        "CLR": Operation.clear
+    ]
+    
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double)
+        case equals
+        case clear
+    }
+    
+    private struct PendingBinaryOperation {
+        let function: (Double, Double) -> Double
+        let firstOperand: Double
+        
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand, secondOperand)
+        }
+    }
+    
+    private mutating func performPendingBinaryOperation() {
+        if pendingBinaryOp != nil && accumulator != nil {
+            accumulator = pendingBinaryOp!.perform(with: accumulator!)
+            pendingBinaryOp = nil
+        }
+    }
+    
+    mutating func setOperand(_ operand: Double) {
+        accumulator = operand
+    }
+    
+    mutating func updateDescription(with symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant:
+                description += "\(symbol)"
+            case .unaryOperation:
+                description =  "\(symbol)(\(description))"
+            case .binaryOperation:
+                if pendingBinaryOp != nil {
+                    description += " \(symbol) "
+                }
+            case .equals:
+                break
+            case .clear:
+                description = ""
+            }
+        } else {
+            description += symbol
+        }
+        print(description)
+    }
+    
+    mutating func performOperation(_ symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+                updateDescription(with: symbol)
+            case .unaryOperation(let function):
+                if accumulator != 0 {
+                    accumulator = function(accumulator!)
+                    pendingBinaryOp = nil
+                    updateDescription(with: symbol)
+                }
+            case .binaryOperation(let function):
+                if accumulator != nil {
+                    pendingBinaryOp = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    accumulator = nil
+                }
+                updateDescription(with: symbol)
+            case .equals:
+                performPendingBinaryOperation()
+                pendingBinaryOp = nil
+                updateDescription(with: symbol)
+            case .clear:
+                pendingBinaryOp = nil
+                accumulator = 0
+                updateDescription(with: symbol)
+            }
+
+        }
+    }
+
+    
+
+}
--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var userTyping = false
+    
+    private var calcModel = CalculatorModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,58 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userTyping {
+            if let currentText = display.text {
+                if digit == "." {
+                    if currentText.range(of:".") == nil {
+                        display.text = currentText + digit
+                        calcModel.updateDescription(with: digit)
+                    }
+                } else {
+                    display.text = currentText + digit
+                    calcModel.updateDescription(with: digit)
+                }
+            }
+        } else {
+            display.text = digit
+            userTyping = true
+            calcModel.updateDescription(with: digit)
+        }
+        setDescriptionLabel(with: calcModel.description)
+    }
+    
+    @IBAction func pressOperation(_ sender: UIButton) {
+        if userTyping {
+            calcModel.setOperand(displayValue)
+            userTyping = false
+        }
+        if let symbol = sender.currentTitle {
+            calcModel.performOperation(symbol)
+        }
+        if let result = calcModel.result {
+            displayValue = result
+        }
+        setDescriptionLabel(with: calcModel.description)
+    }
+    
+    func setDescriptionLabel(with description: String) {
+        if calcModel.resultIsPending {
+            descriptionLabel.text = description + " ..."
+        } else {
+            descriptionLabel.text = description + " ="
+        }
+    }
 }
 
